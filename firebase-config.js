62d99f0189fdae23c97217e7494a6949bf23f042
akinabum <madina.shaliyeva2003@gmail.com>
2025-06-16 20:01:06 +0400

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+// Firebase конфигурация
+module.exports = {
+  apiKey: "AIzaSyDyI2Xvkp6u9fO7T-vsgJLrA8O8A8Y6MS4",
+  authDomain: "sillytavern-storage.firebaseapp.com",
+  projectId: "sillytavern-storage",
+  storageBucket: "sillytavern-storage.appspot.com",
+  messagingSenderId: "65875903165",
+  appId: "1:65875903165:web:2745f919ee4c86a01c6dd4"
+};
